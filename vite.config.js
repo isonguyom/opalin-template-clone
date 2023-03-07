6c4d9c74aa17cd82ca767fc381b9af85265009b2
isonguyom <isonguyom@gmail.com>
2023-03-07 21:58:56 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/opalin-template-clone/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
